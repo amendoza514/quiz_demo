6b5f355b1c37b87df447c11cd6123e3cc3f80db1
amendoza514 <amendoza15@cmc.edu>
2020-11-19 10:28:21 -0800

    clean up

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,18 +14,6 @@ function Navbar() {
                 <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
                   All That Apply&nbsp;
                   <i className="fas fa-check"></i>
-                  {/* <svg 
-                    className='check'
-                    xmlns="http://www.w3.org/2000/svg" 
-                    
-                    width='30' 
-                    height='30'
-                    viewBox="0 0 24 24">
-                    <g fill="#ffffff">
-                      <path 
-                        d="M0 11.386l1.17-1.206c1.951.522 5.313 1.731 8.33 3.597 3.175-4.177 9.582-9.398 13.456-11.777l1.044 1.073-14 18.927-10-10.614z"/>
-                    </g>
-                  </svg> */}
                   </motion.div>
               </Link>
               <div className="menu-icon" onClick={() => setClick(!click)}>
